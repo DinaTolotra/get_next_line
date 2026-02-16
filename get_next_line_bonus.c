e5d6c5ec218f54e0ee57ece15563efc1c294b48b
todina-r <ratsinjomananatolotra@gmail.com>
2026-02-16 10:37:51 +0300

    bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: todina-r <todina-r@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 18:52:25 by todina-r          #+#    #+#             */
+/*   Updated: 2026/02/16 09:42:26 by todina-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+#ifndef EOL_SPEC
+# define EOL_SPEC '\n'
+#endif
+
+#ifndef FD_MAX
+# define FD_MAX 128
+#endif
+
+char	*find_eol(char *p, size_t read_size)
+{
+	char	*p_eol;
+
+	p_eol = ft_strchr(p, EOL_SPEC);
+	if (!p_eol && read_size < BUFFER_SIZE)
+		p_eol = ft_strchr(p, 0);
+	return (p_eol);
+}
+
+char	*overwrite(char *dst, char *src)
+{
+	if (dst)
+		free(dst);
+	dst = src;
+	return (dst);
+}
+
+char	*extract_line(char **buffer, char *p_eol)
+{
+	char	*line;
+	char	*temp;
+	size_t	line_size;
+	size_t	buffer_size;
+
+	temp = 0;
+	line_size = p_eol - *buffer + 1;
+	buffer_size = ft_strlen(*buffer);
+	buffer_size = buffer_size - line_size;
+	line = ft_substr(*buffer, 0, line_size);
+	if (buffer_size)
+		temp = ft_substr(*buffer, line_size, buffer_size);
+	*buffer = overwrite(*buffer, temp);
+	return (line);
+}
+
+size_t	read_data(int fd, char *data)
+{
+	size_t	size;
+
+	size = read(fd, data, BUFFER_SIZE);
+	data[size] = 0;
+	return (size);
+}
+
+char	*get_next_line(int fd)
+{
+	char			data[BUFFER_SIZE + 1];
+	size_t			data_size;
+	static char		**buffer_list;
+	char			*temp;
+	char			*line;
+
+	line = 0;
+	if (!buffer_list)
+		buffer_list = malloc(sizeof(char *) * FD_MAX);
+	while (!line)
+	{
+		temp = 0;
+		data_size = read_data(fd, data);
+		if (!data_size && (!buffer_list[fd] || !buffer_list[fd][0]))
+			return (0);
+		if (buffer_list[fd])
+			temp = ft_strjoin(buffer_list[fd], data);
+		else
+			temp = ft_strjoin("", data);
+		buffer_list[fd] = overwrite(buffer_list[fd], temp);
+		temp = find_eol(buffer_list[fd], data_size);
+		if (temp)
+			line = extract_line(&buffer_list[fd], temp);
+	}
+	return (line);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_util.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 06:26:36 by todina-r          #+#    #+#             */
+/*   Updated: 2026/02/13 06:51:30 by todina-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	index;
+
+	index = 0;
+	while (s[index])
+		index++;
+	return (index);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t		index;
+	const char	*res;
+
+	res = 0;
+	index = 0;
+	while (s[index] && s[index] != (char)c)
+		index++;
+	if (s[index] == (char)c)
+		res = &s[index];
+	return ((char *)res);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	index;
+	size_t	len;
+
+	index = 0;
+	while (index + 1 < size && src[index])
+	{
+		dst[index] = src[index];
+		index++;
+	}
+	if (size)
+		dst[index] = 0;
+	len = index;
+	while (src[len])
+		len++;
+	return (len);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub_s;
+	size_t	s_len;
+
+	if (s == 0)
+		return (0);
+	s_len = ft_strlen(s);
+	if (s_len < start)
+	{
+		len = 0;
+		start = s_len;
+	}
+	else if (s_len - start < len)
+		len = s_len - start;
+	sub_s = malloc(sizeof(char) * (len + 1));
+	if (sub_s == 0)
+		return (0);
+	ft_strlcpy(sub_s, s + start, len + 1);
+	sub_s[len] = 0;
+	return (sub_s);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = 0;
+	if (s1 == 0 || s2 == 0)
+		return (0);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	res = malloc(sizeof(char) * (s2_len + s1_len + 1));
+	if (res == 0)
+		return (0);
+	ft_strlcpy(res, s1, s2_len + s1_len + 1);
+	ft_strlcpy(res + s1_len, s2, s2_len + 1);
+	return (res);
+}
